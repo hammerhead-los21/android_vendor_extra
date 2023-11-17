f7e6c00bccb482c1fa80e89d3d5571fb8b8bf59e
2spirit <2spirit@free.fr>
2023-11-17 18:42:27 +0100

    keep only user-keys config here

--- product.mk
+++ product.mk
@@ -2,12 +2,3 @@
 PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey
 PRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey
 
-# Inherit from proprietary files for miuicamera
--include vendor/xiaomi/sweet-miuicamera/products/board.mk
-
-# Call the MiuiCamera setup
-$(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
-
-# Add ih8sn
-$(call inherit-product-if-exists, ih8sn/product.mk)
-
