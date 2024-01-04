43e87a3efcd015ff5fa14eb5bca34496af801e41
2spirit <2spirit@free.fr>
2024-01-04 15:07:01 +0100

    add ih8sn and OTA in vendor/extra

--- product.mk
+++ product.mk
@@ -2,3 +2,8 @@
 PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey
 PRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey
 
+$(call inherit-product-if-exists, ih8sn/product.mk)
+
+# Properties
+TARGET_SYSTEM_PROP += vendor/extra/system.prop
+
