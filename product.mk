741f03ca814c0164b2a6bd0cedbe71df40be5157
2spirit <2spirit@free.fr>
2024-01-04 15:14:20 +0100

    add miuicamera in vendor/extra

--- product.mk
+++ product.mk
@@ -1,9 +1,16 @@
-
+# my user-keys
 PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey
 PRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey
 
+# add ih8sn
 $(call inherit-product-if-exists, ih8sn/product.mk)
 
-# Properties
+# Inherit from proprietary files for miuicamera
+-include vendor/xiaomi/sweet-miuicamera/products/board.mk
+
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
+
+# add OTA in system properties
 TARGET_SYSTEM_PROP += vendor/extra/system.prop
 
