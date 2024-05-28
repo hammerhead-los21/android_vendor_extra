aa91c6e4cea754537f087032dd17039106339ae6
2spirit <64271596+2spirit@users.noreply.github.com>
2024-05-28 17:25:22 +0200

    Update product.mk

--- product.mk
+++ product.mk
@@ -5,12 +5,5 @@ PRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey
 # add ih8sn
 $(call inherit-product-if-exists, ih8sn/product.mk)
 
-# Inherit from proprietary files for miuicamera
-#-include vendor/xiaomi/sweet-miuicamera/products/board.mk
-
-# Call the MiuiCamera setup
-#$(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
-
 # add OTA in system properties
 TARGET_SYSTEM_PROP += vendor/extra/system.prop
-
